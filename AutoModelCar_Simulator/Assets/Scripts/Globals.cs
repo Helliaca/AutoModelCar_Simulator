4bd8f9afb3ddc7a807356e668f8bfdd814e39da5
Helliaca <benjaminkahl19@gmail.com>
2019-04-23 22:43:33 +0200

    Added rudimentary Developers Console

---
+++ AutoModelCar_Simulator/Assets/Scripts/Globals.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Globals : MonoBehaviour {
+	public static Globals Data;
+	public static DevConsoleController DevConsole;
+
+	void Awake()
+	{
+		if(Data != null) GameObject.Destroy(Data);
+		else Data = this;
+		DontDestroyOnLoad(this);
+	}
+		
+	void Start () {
+	}
+
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Comma)) DevConsole.toggle();
+	}
+}
---
+++ AutoModelCar_Simulator/Assets/Scripts/UI_Scripts/DevConsoleController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class DevConsoleController : MonoBehaviour {
+	private InputField input;
+	private Text output;
+	private GameObject consoleContainer;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(transform);
+	}
+
+	void Start () {
+		Globals.DevConsole = this;
+		consoleContainer = transform.Find("DevConsole").gameObject;
+		output = transform.Find("DevConsole/Output").GetComponent<Text>();
+		input = transform.Find("DevConsole/Input").GetComponent<InputField>();
+		consoleContainer.SetActive(false);
+	}
+
+	void OnGUI() {
+		if(input.isFocused && input.text != "" && Input.GetKey(KeyCode.Return)) {
+			execute(input.text);
+			input.text = "";
+		}
+	}
+
+	public void execute(string command) {
+		if(command == "hi") {
+			print("whats up");
+		}
+		else {
+			print("Command not recognized: " + command);
+		}
+		input.Select();
+		input.ActivateInputField();
+	}
+
+	public void print(string s) {
+		Debug.Log("CONSOLE:" + s);
+		output.text += "\n> " + s;
+	}
+
+	public void show() {
+		consoleContainer.SetActive(true);
+		input.Select();
+		input.ActivateInputField();
+	}
+
+	public void hide() {
+		consoleContainer.SetActive(false);
+	}
+
+	public void toggle() {
+		if(!consoleContainer.activeInHierarchy) show();
+		else hide();
+	}
+}
